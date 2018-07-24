bcc127ff03f74a01000ce5fd332fcc2d05783f6f
James Church <jcchurch@gmail.com>
2018-07-24 13:16:38 -0500

    Restructures the code so that I am not doing so much in a try block.

--- cpp/hflip.cpp
+++ cpp/hflip.cpp
@@ -14,34 +14,40 @@ int main(int argc, char *argv[]) {
   }
   
   PPM_header img_header;
+  std::ifstream ifs;
+  std::ofstream ofs;
   
   try {
-    
-    std::ifstream ifs(argv[1], std::ios::binary);
+    ifs.open(argv[1], std::ios::binary);
     if (!ifs) {
       throw std::runtime_error("Cannot open input file");
     }
     PPM_read_header(ifs, img_header);
     
     std::cout << img_header << std::endl; 
-    
-    RGB_8 *img = new RGB_8[img_header.height * img_header.width];
-    PPM_read_rgb_8(ifs, img_header.width, img_header.height, (RGB_8 *) img);
-
-    std::ofstream ofs(argv[2], std::ios::binary);
-    if (!ofs) {
-      throw std::runtime_error("Cannot open output file");
-    }
-    PPM_write_header_8(ofs, img_header.width, img_header.height);
-    PPM_write_rgb_8(ofs, img_header.width, img_header.height, (RGB_8 *) img);
-    
-    ifs.close();
-    ofs.close();
-    
   } catch (std::runtime_error &re) {
     std::cout << re.what() << std::endl;
     return 2;
   }
+
+  try {
+    ofs.open(argv[2], std::ios::binary);
+    if (!ofs) {
+      throw std::runtime_error("Cannot open output file");
+    }
+  } catch (std::runtime_error &re) {
+    std::cout << re.what() << std::endl;
+    return 2;
+  }
+   
+  RGB_8 *img = new RGB_8[img_header.height * img_header.width];
+  PPM_read_rgb_8(ifs, img_header.width, img_header.height, (RGB_8 *) img);
+
+  PPM_write_header_8(ofs, img_header.width, img_header.height);
+  PPM_write_rgb_8(ofs, img_header.width, img_header.height, (RGB_8 *) img);
+
+  ifs.close();
+  ofs.close();
   return 0;     
 }
 
