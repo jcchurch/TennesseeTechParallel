cfafaa64a510ca37d1ad6fe84aecf8ae3194eb8d
James Church <jcchurch@gmail.com>
2018-07-26 10:19:14 -0500

    Adds HelloOMP

---
+++ java/HelloOMP.java
@@ -0,0 +1,6 @@
+public class HelloOMP {
+     public static void main(String[] args) {
+         //omp parallel
+         System.out.println("Hello world from " + OMP4J_THREAD_NUM + "/" + OMP4J_NUM_THREADS);
+     }
+}
